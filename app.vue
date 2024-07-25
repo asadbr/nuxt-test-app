262c43e7ebfdeb8afe837e0ab547eb88799296c4
Asad <barotov.asadbek@gmail.com>
2024-07-25 10:50:35 +0500

    Nuxt Table

---
+++ app.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="h-full bg-[#fbfbfb]">
+    <div class="pt-60">
+      <UiTable title="Акции" :heads="tableHeads" :items="tableItems">
+        <template #default>
+          <div
+            class="rounded-xl shadow-[0_0_0_2px_#f4f4f4] p-4 mt-6 bg-[#fbfbfb]"
+          >
+            <div class="flex justify-center items-center gap-x-4">
+              <div class="space-x-2">
+                <span class="text-[#9ea1b0] text-xl">Комиссия:</span>
+                <span class="text-[#031138] text-xl">334,43 ₽</span>
+              </div>
+              <span>—</span>
+              <div class="space-x-2">
+                <span class="text-[#9ea1b0] text-xl">Фин. результат:</span>
+                <span class="text-[#031138] text-xl">76 354,82 ₽</span>
+              </div>
+            </div>
+          </div>
+        </template>
+      </UiTable>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const tableHeads = [
+  "Инструмент",
+  "Обновление",
+  "Позиция",
+  "Комиссия (₽)",
+  "Фин. рез. (₽)",
+  "Просадка (₽)",
+  "Плечо",
+];
+
+const tableItems = [
+  {
+    instrument: "Apple",
+    update: "10:00",
+    position: "Buy",
+    commission: "0.1",
+    financialResult: 100,
+    drawdown: "10%",
+    leverage: "1:100",
+  },
+  {
+    instrument: "Google",
+    update: "11:00",
+    position: "Sell",
+    commission: "0.2",
+    financialResult: -50,
+    drawdown: "20%",
+    leverage: "1:200",
+  },
+  {
+    instrument: "Microsoft",
+    update: "12:00",
+    position: "Buy",
+    commission: "0.3",
+    financialResult: 150,
+    drawdown: "30%",
+    leverage: "1:300",
+  },
+];
+</script>
--- components/ui/Table.vue
+++ components/ui/Table.vue
@@ -1,11 +1,82 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  $END$
+  <div class="max-w-[1480px] w-full mx-auto p-6">
+    <h1 class="text-5xl text-gray-800 mb-7">{{ title }}</h1>
+    <div class="bg-white shadow-md rounded-3xl p-6">
+      <table class="min-w-full bg-white">
+        <thead class="bg-white">
+          <tr>
+            <th
+              v-for="(head, index) in heads"
+              :key="index"
+              class="py-2 px-4 text-left text-xl font-normal text-[#9ea1b0]"
+            >
+              {{ head }}
+            </th>
+          </tr>
+        </thead>
+        <tbody class="rounded-xl bg-[#fbfbfb]">
+          <tr v-for="(item, index) in items" :key="index" class="border-0">
+            <td
+              class="p-4 text-xl font-normal text-[#031138] border-0 rounded-xl uppercase"
+            >
+              {{ item.instrument }}
+            </td>
+            <td
+              class="p-4 text-xl font-normal text-[#031138] border-0 rounded-xl"
+            >
+              {{ item.update }}
+            </td>
+            <td
+              class="p-4 text-xl font-normal text-[#031138] border-0 rounded-xl"
+            >
+              {{ item.position }}
+            </td>
+            <td
+              class="p-4 text-xl font-normal text-[#031138] border-0 rounded-xl"
+            >
+              {{ item.commission }}
+            </td>
+            <td
+              :class="[
+                'p-4 text-xl border-0 rounded-xl',
+                item.financialResult > 0 ? 'text-green-600' : 'text-red-600',
+              ]"
+            >
+              {{ item.financialResult }}
+            </td>
+            <td
+              class="px-4 text-xl font-normal text-[#031138] border-0 rounded-xl"
+            >
+              {{ item.drawdown }}
+            </td>
+            <td
+              class="p-4 text-xl font-normal text-[#031138] border-0 rounded-xl"
+            >
+              {{ item.leverage }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <slot />
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+type TItem = {
+  instrument: string;
+  update: string;
+  position: string;
+  commission: string;
+  financialResult: number;
+  drawdown: string;
+  leverage: string;
+};
+type TProps = {
+  title?: string;
+  heads: string[];
+  items: TItem[];
+};
 
-</style>
+defineProps<TProps>();
+</script>
